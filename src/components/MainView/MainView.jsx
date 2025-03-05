7fac589fe57d9eeab5b2d8acbd841e514985064b
agawi88 <agnessn@icloud.com>
2025-03-05 16:11:44 +0100

    Updated MainView
    removed the hardcoded movie data and tried to connect the movie Data from my  movie_api using the method used in the exercise in SandBox (useEffect)

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -3,63 +3,31 @@ import { MovieCard } from "../MovieCard/MovieCard";
 import { MovieView } from "../MovieView/MovieView";
 
 const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Atonement",
-      releaseYear: "2007",
-      setting: "United Kingdom, France",
-      description:
-        "A dramatic tale of love and war, set in the UK and France during World War II, based on Ian McEwan's novel.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/e/e4/Atonement_UK_poster.jpg",
-      director: "Joe Wright",
-      genre: "Drama",
-      featured: true,
-    },
-    {
-      id: 2,
-      title: "Belfast",
-      releaseYear: "2021",
-      setting: "Northern Ireland, Belfast",
-      description:
-        "A young boy and his working-class Belfast family experience the tumultuous late 1960s.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/4/4a/Belfast_poster.jpg",
-      director: "Kenneth Branagh",
-      genre: "Drama",
-      featured: true,
-    },
-    {
-      id: 3,
-      title: "Black Doves",
-      releaseYear: "2024",
-      setting: "England, London",
-      description:
-        "Helen embarks on a passionate affair with a man who has no idea what her secret identity is. Caught in the crosshairs when her lover falls victim to the dangerous London underworld, Helen's employers call in Sam to protect her.",
+  const [movies, setMovies] = useState([  ]);
 
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/c/c1/Black_Doves.jpg",
-      director: "Lisa Gunning",
-      genre: "Thriller",
-      featured: false,
-    },
-    {
-      id: 4,
-      title: "Bridget Jones/' Diary",
-      releaseYear: "2001",
-      setting: "England, London",
-      description:
-        "Bridget Jones is determined to improve herself while she looks for love in a year in which she keeps a personal diary.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/1/17/BridgetJonesDiaryMoviePoster.jpg",
-      director: "Sharon Maguire",
-      genre: "Comedy",
-      featured: true,
-    },
-  ]);
+const [selectedMovie, setSelectedMovie] = useState(null);
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
+    useEffect(() => {
+    fetch("https://british-movies-23cb3bbeb9f8.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            releaseYear: doc.releaseYear,
+            setting: doc.setting,
+            description: doc.description,
+            director: doc.director_name?.[0],
+            genre: doc.genre_name?.[0],
+            featured: doc.featured,
+            image: ,
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
