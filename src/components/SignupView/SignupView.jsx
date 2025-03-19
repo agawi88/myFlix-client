dc1ad123c6fbe788b02098fb7e5190aa1085eb4b
agawi88 <agnessn@icloud.com>
2025-03-19 10:37:00 +0100

    Updated signupView.jsx
    added the necessary data  to variable data, added necessary variables to export const SignupView, added and if-else statement, adjusted the signup form

--- src/components/SignupView/SignupView.jsx
+++ src/components/SignupView/SignupView.jsx
@@ -1,79 +1,83 @@
 import React from "react";
+import { useState } from "react";
+
 export const SignupView = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [dateOfBirth, setDateOfBirth] = useState("");
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
         const data = {
-            access: username,
-            secret: password,
+            Username: username,
+            Password: password,
+            Email: email,
+            DateOfBirth: dateOfBirth,
         };
         fetch("https://british-movies-23cb3bbeb9f8.herokuapp.com/users", {
           method: "POST",
-          headers: {
-              "Content-Type": "application/json"
-          },
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify(data),
       })
           .then((response) => response.json())
-          .then((data) => {
-              console.log("Signup response: ", data);
-              if (data.user) {
-                  localStorage.setItem("user", JSON.stringify(data.user));
-                  localStorage.setItem("token", data.token);
-                  onLoggedIn(data.user, data.token);
-              } else {
-                  alert("No such user");
-              }
-          })
+            .then((data) => {
+                console.log("Signup response: ", data);
+                if (data.message === "User already exists") {
+                    alert("Username already exists! Choose a different username OR log in.");
+                } else {
+                    alert("Signup successful! Please log in.");
+                }
+            })
           .catch((e) => {
               alert("Something went wrong");
           });
   };
-    };
 
     return (
         <form onSubmit={handleSubmit}>
             <label>
                 Username:
-                <input type="text" />
+                <input
+                type="text" 
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 required
                 minlength="5"
                 maxlength="12"
+                />
             </label>
             <label>
-                Password:
+                Password (8 characters minimum):
               <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          minlength="8"
-          maxlength="15"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                minlength="8"
+                maxlength="15"
         />
             </label>
             <label> 
                 Email:
               <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          minlength="8"
-          maxlength="15"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                pattern="^[^@\s]+@[^@\s]+\.[^@\s]+$" size="30"
+                placeholder="sophie@example.com"
         />
             </label>
             <label>
                 Date of Birth:
               <input
-          type="passworddateOfBirth"
-          value={dateOfBirth}
-          onChange={(e) => setDateOfBirth(e.target.value)}
-          required
+                type="date"
+                value={dateOfBirth}
+                onChange={(e) => setDateOfBirth(e.target.value)}
+                required
+                placeholder="01-01-2000"
         />
             </label>
             <button type="submit">Submit</button>
