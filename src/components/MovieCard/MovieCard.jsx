02ae500a555b4ab87f882fb86f7e2e7563a5bbb8
agawi88 <agnessn@icloud.com>
2025-04-02 12:25:02 +0200

    Updated  MovieCard.jsx
    changed react elements to react bootstrap Form elements

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,14 +1,16 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+import "./MovieCard.scss";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.title}
-    </div>
+    <Card className="h-100">
+    <Card.Body>
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Title> {movie.title} </Card.Title>
+        <Button onClick={() => onMovieClick(movie)} variant="link"> Open </Button>
+    </Card.Body>
+    </Card>
   );
 };
 
