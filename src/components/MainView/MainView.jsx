79fdcc7dc190377eb9e84d99469c5ac7b2d8ee90
agawi88 <agnessn@icloud.com>
2025-04-10 16:16:09 +0200

    Updated MainView.jsx
    imported three new Views, react-icons, added new states for adding movies to favorites, added toggleFavorite, added favorite button to the Navbar and chained the buttons together in a ButtonGroup, added comments to make the code more readable by naming different sections, cleaned up the code a little, added a modal for dispalying the MovieView, added some grid and responsive css

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -3,9 +3,16 @@ import { MovieCard } from "../MovieCard/MovieCard";
 import { MovieView } from "../MovieView/MovieView";
 import { LoginView } from "../LoginView/LoginView";
 import { SignupView } from "../SignupView/SignupView";
+import { SimilarMoviesByGenre } from "../SimilarMovies/SimilarMoviesByGenre";
+import { SimilarMoviesByDirector } from "../SimilarMovies/SimilarMoviesByDirector";
+import { FavoritesView } from "../FavoriteMovies/FavoritesView";
+
+
 import PropTypes from "prop-types";
 
-import { Container, Row, Col, Button, Form, Navbar } from "react-bootstrap";
+import { Container, Row, Col, Button, ButtonGroup, Form, Navbar, Modal } from "react-bootstrap";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+
 
 
 export const MainView = () => {
@@ -16,7 +23,12 @@ export const MainView = () => {
   const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
-  const [showSignup, setShowSignup] = useState(false);
+  const [currentView, setCurrentView] = useState("main");
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const [favoriteMovieIds, setFavoriteMovieIds] = useState([]);
+
+  const isFavorite = favoriteMovieIds.includes(selectedMovie?.id);
+  // const [showSignup, setShowSignup] = useState(false);
 
   useEffect(() => {
     console.log(user, token);
@@ -56,11 +68,37 @@ export const MainView = () => {
       });
   }, [user, token]);
 
+  let similarMoviesByGenre = [];
+  let similarMoviesByDirector = [];
+
+  if (selectedMovie) {
+    
+    similarMoviesByGenre = movies.filter(
+      (movie) =>
+        movie.genre.name === selectedMovie.genre.name &&
+        movie.id !== selectedMovie.id
+    );
+    
+    similarMoviesByDirector = movies.filter(
+      (movie) =>
+        movie.director.name === selectedMovie.director.name &&
+        movie.id !== selectedMovie.id
+    );
+  }
+
+  let toggleFavorite = (movieId) => {
+    setFavoriteMovieIds((prev) =>
+      prev.includes(movieId)
+        ? prev.filter((id) => id !== movieId)
+        : [...prev, movieId]
+    );
+  };
+
   return (
     <Container>
       {user && (selectedMovie || movies.length > 0) && (
-
-<Navbar className="Navbar justify-content-between">
+//  NVBAR
+    <Navbar sticky="top" className="Navbar justify-content-between">
       <Form className="d-flex">
         <Row>
           <Col xs="auto">
@@ -74,116 +112,134 @@ export const MainView = () => {
             <Button type="submit" className="RegularButton">Submit</Button>
           </Col>
         </Row>
-      </Form>
-          <Form className="d-flex">
-        <Col className="d-grid gap-2 d-md-flex justify-content-md-end">
-                 <Button className="btn RegularButton me-md-2" type="button"
-          onClick={() => {
-          setUser(null);
-          setToken(null);
-          localStorage.setItem("user", JSON.stringify(user)); 
-          localStorage.setItem("token", token);
-          //setShowSignup(false); // Reset to login screen
-          }}
-        >
-          Logout
-        </Button>
-      </Col>
-      </Form>
-    </Navbar>
+          </Form>
+        <Form className="d-flex justify-content-md-end">
+            <Row>
+            <ButtonGroup>
+              <Col xs="auto">
+                <Button onClick={() =>
+                setCurrentView("favorites")}>
+                Favorites
+                </Button>
+                </Col>
+                <Col xs="auto">
+                  {currentView === "favorites" && (
+                    <Button onClick={() =>
+                      setCurrentView("main")} className="mb-3">
+                      Back to Home
+                    </Button>
+                  )}
+                </Col>
+              <Col xs="auto">
+                <Button className="btn RegularButton me-md-2" type="button"
+                onClick={() => {
+                setUser(null);
+                setToken(null);
+                localStorage.setItem("user", JSON.stringify(user)); 
+                localStorage.setItem("token", token);
+              // setShowSignup(false); // Reset to login screen
+              }}
+                >
+                Logout
+              </Button>
+                </Col>
+              </ButtonGroup>
+          </Row> 
+    </Form>
+  </Navbar>
       )}
-       
-      <Row className="justify-content-md-center">
-    {!user ? (
-          <Col md={5} mb-1 p-2>
-            <Row>
-      <LoginView
-      onLoggedIn={(user, token) => {
-      setUser(user);
-      setToken(token);
-      localStorage.setItem("user", JSON.stringify(user)); 
-      localStorage.setItem("token", token);
-      }}
-              />
-            </Row>
-            <Row>
-              <SignupView />
-            </Row>
-      </Col> 
-      ) : selectedMovie ? (
-          (() => {
-        // Move logic outside JSX
-        let similarMoviesByGenre = movies.filter(
-          (movie) => movie.genre.name === selectedMovie.genre.name && movie.id !== selectedMovie.id
-        );
-        let similarMoviesByDirector = movies.filter(
-          (movie) => movie.director.name === selectedMovie.director.name && movie.id !== selectedMovie.id
-        );
-
-        return (
-          <Col md={8} style={{ border: "1px solid green"}} >
-      <MovieView
-        movie={selectedMovie}
-        movies={movies} // for a full list of movies
-        onBackClick={() => setSelectedMovie(null)}
-        onMovieClick={() => setSelectedMovie(selectedMovie)}
-        />
-        <hr />
-        <h2> Similar Movies </h2>
-        <h3>By Genre</h3>
-        {similarMoviesByGenre.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            movie={movie}    // Pass the movie as a prop to MovieCard
-            onMovieClick={() => {
-            setSelectedMovie(movie);
-          }}  // Pass the onMovieClick function
-          />
-        ))}
-        <hr />
-        <h3>By Director</h3>
-        {similarMoviesByDirector.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            movie={movie}
-            onMovieClick={() => {
-            setSelectedMovie(movie);
-          }}
-          />
-        ))}
-       
-        {/* For future embellishing, once actors will be added, also Similar Movies by Actors:
-         <hr />
-        <h3>By Actors</h3>
-        {similarMoviesByActors.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            movie={movie}
-            onMovieClick={() => {
-            setSelectedMovie(movie);
-          }}
-          />
-        ))}}  */}      
-          </Col>
-       );
-      })()
-  ) : movies.length === 0 ? (
+{/* // MAIN and OTHER VIEWs */}
+      {!user ? (
+        <Col md={5} mb-1 p-2>
+          <Row>
+            <LoginView
+              onLoggedIn={(user, token) => {
+                setUser(user);
+                setToken(token);
+                localStorage.setItem("user", JSON.stringify(user));
+                localStorage.setItem("token", token);
+              }}
+            />
+          </Row>
+          <Row>
+            <SignupView />
+          </Row>
+        </Col>
+// FAVORITES VIEW
+      ) : currentView === "favorites" ? (
+          <>
+            <FavoritesView
+              favoriteMovies={favoriteMovies}
+              onMovieClick={(movie) => {
+                setSelectedMovie(movie);
+                setCurrentView("main");
+              }}
+            />
+          </>   
+        ) : selectedMovie ? (
+// MovieView MODAL bzw. SELECTED MOVIE
+        <Modal
+          show={!!selectedMovie}
+          onHide={() => setSelectedMovie(null)}
+          size="xl"
+          centered
+          scrollable
+          dialogClassName="modal-90w">
+          <Modal.Header>
+                <Modal.Title>{selectedMovie.title}
+                  <Button
+                    variant="link"
+                    onClick={() => toggleFavorite(selectedMovie.id)}
+                    className="ms-3 p-0"
+                    style={{ color: isFavorite ? "red" : "gray", fontSize: "1.5rem"}}
+                  >
+                    {isFavorite ? <FaHeart /> : <FaRegHeart />} 
+                  </Button>
+              </Modal.Title>
+              <Button //onClick={onBackClick}
+                onClick={() => setSelectedMovie(null)}
+                className="backButton ms-auto" 
+                style={{cursor: "pointer"}}
+              >Back</Button>
+          </Modal.Header>
+          <Modal.Body>
+            <MovieView
+              movie={selectedMovie}
+              movies={movies} // for a full list of movies
+              onBackClick={() => setSelectedMovie(null)}
+              onMovieClick={() => setSelectedMovie(selectedMovie)}
+                />
+{/* // SIMILAR MOVIES BY: GENRE, DIRECTOR; later on also by ACTORS AND MY FAVOURITES           */}
+            <hr />
+            <h2> Similar Movies </h2>
+            <SimilarMoviesByGenre
+              movies={similarMoviesByGenre}
+              onMovieClick={(movie) => setSelectedMovie(movie)}
+            />
+            <hr />
+            <SimilarMoviesByDirector
+              movies={similarMoviesByDirector}
+              onMovieClick={(movie) => setSelectedMovie(movie)}
+            />
+          </Modal.Body>
+            </Modal>
+// EMPTY MOVIES LIST
+      ) : movies.length === 0 ? (
         <div>The list is empty!</div>
-          ) : (
-      <Row className="g-4" mb={3}>
+      ) : (
+// MOVIE CARDS IN MAIN VIEW bzw LIST OF MOVIES
+        <Row className="g-4" xs={1} sm={2} md={3} lg={4} mb={3}>
           {movies.map((movie) => (
             <Col key={movie._id} xs={12} sm={6} md={4} lg={3} >
               <MovieCard
-           // key={movie.id}
-              movie={movie}
-              onMovieClick={() => setSelectedMovie(movie)}
+                movie={movie}
+                onMovieClick={() => setSelectedMovie(movie)}
               />
             </Col>
           ))}
-      </Row>
-      )}
-    </Row>
-  </Container >
+        </Row>
+      )}       
+    </Container >
   );
 };
 
