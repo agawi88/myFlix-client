47bde518639d25cc074386eb885a4887a688c720
agawi88 <agnessn@icloud.com>
2025-05-16 14:54:16 +0200

    Updated SimilarMoviesBySections
    moved the filtering code from MainView as it didn't make sense for me there. It felt more logical to have this code in this file and call it in MovieView which in turn is being called in MainView.

--- src/components/SimilarMovies/SimilarMoviesBySections.jsx
+++ src/components/SimilarMovies/SimilarMoviesBySections.jsx
@@ -1,24 +1,49 @@
-import React from "react";
 import PropTypes from "prop-types";
 import { CardGroup, Row, Col, Carousel } from "react-bootstrap";
 // import ExampleCarouselImage from 'components/ExampleCarouselImage';
 import { MovieCard } from "../MovieCard/MovieCard";
 
-export const SimilarMoviesBySections = ({ movies, title, onMovieClick }) => {
+export const SimilarMoviesBySections = ({ movie, movies, title, onMovieClick, selectedMovie }) => {
+    if (!selectedMovie) return null;
     
+  const similarMovies =
+    title === "By Genre"
+      ? movies.filter(
+        (movie) =>
+          movie.genre.name === selectedMovie.genre.name &&
+          movie.id !== selectedMovie.id
+      )
+      : title === "By Director"
+    
+        // const moviesByDirector =
+    
+        ? movies.filter(
+          (movie) =>
+            movie.director.name === selectedMovie.director.name &&
+            movie.id !== selectedMovie.id
+        )
+        : [];
+
+/*     const filteredMovies = title.toLowerCase().includes("genre", "director")
+        ? moviesByGenre
+        : moviesByDirector; */
+  console.log("Similar movies for", title, similarMovies);
+  
     return (
         <>
             <h3>{title}</h3>
             <CardGroup>
-                <Row className="g-3">
-                    {movies.map((movie) => (
+          <Row className="g-3">
+            {similarMovies.map((movie) =>
+          movie ?  (
                         <Col xs={12} sm={6} md={4} lg={3} key={movie.id}>
                     <MovieCard
-                        movie={movie}    // Pass the movie as a prop to MovieCard
-                        onMovieClick={() => onMovieClick(movie)}  // Pass the onMovieClick function
+                        movie={movie}   
+                        onMovieClick={() => onMovieClick(movie)}  
                     />
-                        </Col>
-                ))}
+            </Col>
+          ) : null
+                )}
                 </Row>
             </CardGroup>
         </>
@@ -26,7 +51,27 @@ export const SimilarMoviesBySections = ({ movies, title, onMovieClick }) => {
 };
 
 SimilarMoviesBySections.propTypes = {
+    movie: PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          title: PropTypes.string.isRequired,
+          releaseYear: PropTypes.string.isRequired,
+          setting: PropTypes.string,
+          description: PropTypes.string.isRequired,
+          image: PropTypes.string.isRequired,
+          featured: PropTypes.bool.isRequired,
+          director: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            bio: PropTypes.string.isRequired,
+            dateOfBirth: PropTypes.string.isRequired,
+            deathYear: PropTypes.string,
+          }).isRequired,
+          genre: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+          }).isRequired,
+        }).isRequired,
     movies: PropTypes.array.isRequired,
     title: PropTypes.string.isRequired,
-  onMovieClick: PropTypes.func.isRequired,
+    onMovieClick: PropTypes.func.isRequired,
+  selectedMovie: PropTypes.object.isRequired
 };
