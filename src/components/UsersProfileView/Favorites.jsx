e95745d659b886e29afd52f98896f4ef4f9ee9f3
agawi88 <agnessn@icloud.com>
2025-05-18 03:29:07 +0200

    updated Favorite.jsx
    created a kind of utility file for togglingFavorites, but sth is not entirely right with the props etc. so for the moment this file unused until the I figure out the error

--- src/components/UsersProfileView/Favorites.jsx
+++ src/components/UsersProfileView/Favorites.jsx
@@ -1,50 +1,52 @@
-// import PropTypes from "prop-types";
-// import { useEffect, useState } from "react";
-// import { Row, Col } from "react-bootstrap";
-// import { MovieCard } from "../MovieCard/MovieCard";
+import PropTypes from "prop-types";
+// import { useState } from "react";
 
-// export const Favorites = ({ user, movies, onFavoritedMovie }) => {
-//   const token = localStorage.getItem("token");
-//   const [profileUser, setProfileUser] = useState(null);
-
-//   const favoriteMovies = movies.filter((movie) => (movies.favorite));
-
-//   const favoriteMovieCard = favoriteMovies.map((movie) => (
-//     <MovieCard
-//       key={movie.id}
-//       movie={movie}
-//       onFavoritedMovie={onFavoritedMovie}
-//     />
-//   ))
-
-//   return (
-//     <div>
-//       <div>
-//         <h2>Favorite Movies</h2>
-//       </div>
-//       <div>
-//         <ul className="favorites-container">{favoriteMovieCard}</ul>
-//       </div>
-//     </div>
-//   );
-// }
-// //   useEffect(() => {
-
-// //     const method = isFav ? "POST" : "PUT";
+export const ToggleFavorites = ({ user, token, isFavorited }) => {
   
-// //     fetch(`https://gb-movies-api-cab43a70da98.herokuapp.com/users/${user.Username}/movies/${MovieId}`, {
-// //       method: method,
-// //       headers: {
-// //         Authorization: `Bearer ${token}`,
-// //         "Content-Type": "application/json",
-// //       }
-// //     })
+    if (!user || !user.Username || !movie || !movie._id)
+        return;
 
-// // };
+    
+    const url = `https://gb-movies-api-cab43a70da98.herokuapp.com/users/${user.Username}/movies/${movie.id}`;
+      
+    const options = {
+      method: isFavorited ? "PUT" : "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      }
+    };
 
-// MovieCard.propTypes = {
-//   user: PropTypes.object.isRequired,
-//   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-//   onMovieClick: PropTypes.func.isRequired,
-//   onFavoritedMovie: PropTypes.func.isRequired,
-// };
+    fetch(url, options)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Failed to update favorites");
+            }
+            return response.json();
+        });
+  };
+
+ToggleFavorites.propTypes = {
+    movie: PropTypes.shape({
+            id: PropTypes.string.isRequired,
+          title: PropTypes.string.isRequired,
+          releaseYear: PropTypes.string,
+          setting: PropTypes.string,
+          description: PropTypes.string,
+          genre: PropTypes.shape({
+            name: PropTypes.string,
+            description: PropTypes.string,
+          }),
+          director: PropTypes.shape({
+            name: PropTypes.string,
+            bio: PropTypes.string,
+            dateOfBirth: PropTypes.string,
+            deathYear: PropTypes.string,
+          }),
+          image: PropTypes.string.isRequired,
+          featured: PropTypes.bool,
+        }).isRequired,
+    user: PropTypes.object.isRequired,
+    token: PropTypes.string.isRequired,
+    isFavorited: PropTypes.func.isRequired,
+};
